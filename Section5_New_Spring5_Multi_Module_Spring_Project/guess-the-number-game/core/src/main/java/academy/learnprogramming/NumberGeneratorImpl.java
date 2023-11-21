a0f25366e04f6f9dff365e78212d41270c5a1e45
etienne <etiennekoa6@gmail.com>
2023-11-21 21:17:41 +0100

    spring framework 5

---
+++ Section5_New_Spring5_Multi_Module_Spring_Project/guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+// Created By Etienne KOA
+////////////////////////////////////////////////////////////////////////////////
+
+package academy.learnprogramming;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component("numberGenerator")
+public class NumberGeneratorImpl implements NumberGenerator{
+
+	// == fields ==
+	private final Random random = new Random();
+	private int maxNumber = 100;
+
+	// == public methods ==
+	@Override
+	public int next() {
+		return random.nextInt(maxNumber);
+	}
+
+	@Override
+	public int getMaxNumber() {
+		return maxNumber;
+	}
+}
---
+++ Section5_New_Spring5_Multi_Module_Spring_Project/readme.md
@@ -0,0 +1,4 @@
+### **mvnrepository**
+
++ https://mvnrepository.com/artifact/ch.qos.logback/logback-classic/1.2.3
++ https://mvnrepository.com/artifact/org.springframework/spring-context
